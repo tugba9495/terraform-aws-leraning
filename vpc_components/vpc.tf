006259f029e41a7417e0c005380764bf0ce59a6f
tugba9495 <tuba_7655@icloud.com>
2023-06-25 21:42:10 -0500

    modified vpc_components.tf

--- vpc_components/vpc.tf
+++ vpc_components/vpc.tf
@@ -57,4 +57,13 @@ resource "aws_route_table" "public_route_table" {
       Name = "public_route_table"
     }
 }
+ resource "aws_route_table" "private_route_table" {
+    vpc_id = aws_vpc.my_aws_vpc.id
+    tags = {
+      Name = "private_route_table"
+      
+    }
+   
+ }
+
 
