dcec9b730a6311b9139f35f10914a05a5ab88ee3
tugba9495 <tuba_7655@icloud.com>
2023-06-25 20:31:21 -0500

    modified vpc_components.tf

--- vpc_components/vpc.tf
+++ vpc_components/vpc.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "my_aws_vpc" {
 resource "aws_subnet" "public_subnet1" {
   vpc_id            = aws_vpc.my_aws_vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-west-1a"
+  availability_zone = "us-east-1a"
   tags = {
     Name = "public_subnet_1"
   }
@@ -18,7 +18,7 @@ resource "aws_subnet" "public_subnet1" {
 resource "aws_subnet" "public_subnet2" {
   vpc_id            = aws_vpc.my_aws_vpc.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-west-1c"
+  availability_zone = "us-east-1c"
   tags = {
     Name = "public_subnet_2"
   }
@@ -27,7 +27,7 @@ resource "aws_subnet" "public_subnet2" {
 resource "aws_subnet" "private_subnet1" {
   vpc_id            = aws_vpc.my_aws_vpc.id
   cidr_block        = "10.0.3.0/24"
-  availability_zone = "us-west-1a"
+  availability_zone = "us-east-1a"
   tags = {
     Name = "private_subnet_1"
   }
@@ -37,7 +37,7 @@ resource "aws_subnet" "private_subnet1" {
 resource "aws_subnet" "private_subnet2" {
   vpc_id            = aws_vpc.my_aws_vpc.id
   cidr_block        = "10.0.4.0/24"
-  availability_zone = "us-west-1c"
+  availability_zone = "us-east-1c"
   tags = {
     Name = "private_subnet_2"
   }
